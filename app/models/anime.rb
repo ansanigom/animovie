41743cb4017271c2c354b14a016ff77576e164af
Anju <88181896+ansanigom@users.noreply.github.com>
2022-01-17 00:04:31 +0900

    models validation added

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,2 +1,6 @@
-class Movie < ApplicationRecord
+class Anime < ApplicationRecord
+  belongs_to :list
+
+  validates :title, presence: true, uniqueness: true
+  validates :released, length: { is: 4 }, numericality: { only_integer: true }
 end
--- db/migrate/20220116143954_create_lists.rb
+++ db/migrate/20220116143954_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
       t.string :name
-      t.bigint :movie_id, null: false
+      t.bigint :movie_id, null: false, foreign_key: true
 
       t.timestamps
     end
